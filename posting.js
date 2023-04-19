ab28bb580dbb23210f103a1217559fd89e27f511
killerwhale915 <kimjiwoo9622@naver.com>
2023-04-19 17:56:57 +0900

    Update

---
+++ posting.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("text").value;
+    document.getElementById("log").innerHTML =
+        "이름: " + name + "<br>글 내용 : " + text;
+}
