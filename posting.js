d2051403f877bd01cf2f541a9d3283cc3db16d12
killerwhale915 <kimjiwoo9622@naver.com>
2023-04-19 18:09:56 +0900

    Update

--- posting.js
+++ posting.js
@@ -1,6 +1,8 @@
 function submitForm() {
     var name = document.getElementById("name").value;
     var text = document.getElementById("text").value;
+    localStorage.setItem("name", name);
+    localStorage.setItem("text", text);
     var logElement = document.getElementById("log");
     logElement.innerHTML += "이름: " + name + "<br>글 내용: " + text + "<hr>";
 }
