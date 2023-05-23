8a0cb3637f787d753f61798c8d1e0c61d631b3a1
killerwhale915 <kimjiwoo9622@naver.com>
2023-05-23 11:51:49 +0900

    Update

--- ABC.js
+++ ABC.js
@@ -10,22 +10,3 @@ function enterQuiz() {
     } else {
     }
 }
-
-//a와 b를 더하는 함수
-function addNumbers(a, b) {
-    return a + b;
-}
-
-//a와 b중 더 큰 수를 저장하는 함수
-function getMaxNumber(a, b) {
-    return Math.max(a, b);
-}
-function displayResults() {
-    const sum = addNumbers(5, 3);
-    const maxNumber = getMaxNumber(8, 3);
-    document.getElementById("sumResult").textContent = sum;
-    document.getElementById("MaxResult").textContent = maxNumber;
-}
-
-// displayResults 함수 실행
-displayResults();
