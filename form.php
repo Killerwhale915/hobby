b31a1dac1db9ab09963e6a0bbd73f7538d116a70
killerwhale915 <kimjiwoo9622@naver.com>
2023-05-30 09:58:14 +0900

    Update

--- form.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_GET["name"]) && isset($_GET["text"])){
-    $name = $_GET["name"];
-    $text = $_GET["text"];
-
-    echo "이름: " . $name . "<br>";
-    echo "글 내용: " . $text . "<br>";
-}
-?>
